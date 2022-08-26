909c60b827e427329dbe53fbd3ee9d1728b3d72a
Kyle McLaren <kyle.mclaren@elastic.co>
2022-08-26 17:03:57 +0200

    bump provider versions

--- providers.tf
+++ providers.tf
@@ -2,25 +2,21 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.18.0"
+      version = "4.33.0"
     }
     kubernetes = {
       source  = "hashicorp/helm"
-      version = "2.5.1"
+      version = "2.6.0"
       source  = "hashicorp/kubernetes"
-      version = ">= 2.10.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "= 1.14.0"
+      version = "2.13.0"
     }
   }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = "https://${google_container_cluster._.endpoint}"
-#    config_path    = "~/.kube/config"
+    host = "https://${google_container_cluster._.endpoint}"
+    #    config_path    = "~/.kube/config"
 
     client_certificate     = base64decode(google_container_cluster._.master_auth.0.client_certificate)
     client_key             = base64decode(google_container_cluster._.master_auth.0.client_key)
@@ -29,19 +25,11 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  host                   = "https://${google_container_cluster._.endpoint}"
-#  config_path    = "~/.kube/config"
+  host = "https://${google_container_cluster._.endpoint}"
+  #  config_path    = "~/.kube/config"
 
   client_certificate     = base64decode(google_container_cluster._.master_auth.0.client_certificate)
   client_key             = base64decode(google_container_cluster._.master_auth.0.client_key)
   cluster_ca_certificate = base64decode(google_container_cluster._.master_auth.0.cluster_ca_certificate)
 
-}
-
-
-provider "kubectl" {
-  host                   = "https://${google_container_cluster._.endpoint}"
-  client_certificate     = base64decode(google_container_cluster._.master_auth.0.client_certificate)
-  client_key             = base64decode(google_container_cluster._.master_auth.0.client_key)
-  cluster_ca_certificate = base64decode(google_container_cluster._.master_auth.0.cluster_ca_certificate)
 }
